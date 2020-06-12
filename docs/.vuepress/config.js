c540e3ff7205d5e0ffb6e8f2a8e145f403084ef1
Lemon <30290664+MissQinMeng@users.noreply.github.com>
2020-06-12 17:47:29 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: '微笑的蛋壳呀',
-  description: '饭要一口一口吃，路要一步一步走',
+  title: 'Lemon',
+  description: '哎呀哎呀梦梦呀',
   serviceWorker: true,
   // head: [ // 注入到当前页面的 HTML <head> 中的标签
   //   ['link', { rel: 'icon', href: '/img/head.jpeg' }], // 标签里的头像
@@ -16,7 +16,7 @@ module.exports = {
       {
         text: '链接', 
         items: [
-          {text: 'csdn', link: 'https://blog.csdn.net/sherry_chan'},
+          {text: 'csdn', link: 'https://mp.csdn.net/console/article'},
           {text: 'github', link: 'https://github.com/MissQinMeng/'}     
         ]
       }
